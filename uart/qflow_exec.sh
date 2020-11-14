f2dbd193e9576fe3906e3626651fc591e34b8ef9
Brandon Esquivel <brandon.esquivel@ucr.ac.cr>
2020-11-14 12:28:56 -0600

    finals results

---
+++ uart/qflow_exec.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/tcsh -f
+#-------------------------------------------
+# qflow exec script for project /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart
+#-------------------------------------------
+
+/usr/lib/qflow/scripts/synthesize.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+/usr/lib/qflow/scripts/placement.sh -d /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+/usr/lib/qflow/scripts/vesta.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+# /usr/lib/qflow/scripts/router.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+# /usr/lib/qflow/scripts/placement.sh -f -d /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+# /usr/lib/qflow/scripts/router.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1 $status
+# /usr/lib/qflow/scripts/cleanup.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
+# /usr/lib/qflow/scripts/display.sh /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart uart || exit 1
---
+++ uart/qflow_vars.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/tcsh -f
+#-------------------------------------------
+# qflow variables for project /home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart
+#-------------------------------------------
+
+set projectpath=/home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart
+set techdir=/usr/share/qflow/tech/osu050
+set sourcedir=/home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart/source
+set synthdir=/home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart/synthesis
+set layoutdir=/home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart/layout
+set techname=osu050
+set scriptdir=/usr/lib/qflow/scripts
+set bindir=/usr/lib/qflow/bin
+set synthlog=/home/bran/Documentos/repositories/Qflow_Synthesis_physical_implementation/uart/synth.log
+#-------------------------------------------
+
